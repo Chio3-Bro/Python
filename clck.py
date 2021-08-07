93673fda8312b4e2b0f709a4ece8083ea528ea25
Chio-Bro <87470626+Chio3-Bro@users.noreply.github.com>
2021-08-07 12:43:04 +0300

    Update clck.py

--- clck.py
+++ clck.py
@@ -1,17 +1,22 @@
+#Счётчик кликов для майнкрафт-юзеров
+
 from tkinter import *
- 
+
+#Кол-во кликов
 clicks = 0
  
- 
+#Фун-ия по клику
 def click_button():
     global clicks
     clicks += 1
     root.title("Clicks {}".format(clicks))
  
+#Параметры переменной
 root = Tk()
 root.title("WinHelpClicks")
 root.geometry("220x50")
  
+#Сама кнопка
 btn = Button(text="Click Me", background="#555", foreground="#ccc",
              padx="20", pady="8", font="16", command=click_button).pack()
 
